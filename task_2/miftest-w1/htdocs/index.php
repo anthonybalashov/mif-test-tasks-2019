06f540ae24ed62939b7da1c3851a2de93930dc79
Anthony Balashov <anthonybalashov@gmail.com>
2020-07-29 22:54:54 +0300

    upload Test tasks to git

---
+++ task_2/miftest-w1/htdocs/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<b>Host: </b>miftest-w1 <b>container name:</b> \n";
+echo gethostname();
+phpinfo();
+?>
---
+++ task_2/miftest-w2/htdocs/index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<b>Host: </b>miftest-w2 <b>container name:</b> \n"; 
+echo gethostname();
+phpinfo();
+?>
